190480d98c5828e96445c43f642c75dab787a695
meait-3 <tchikh3@@gmail.com>
2023-03-11 13:00:53 +0100

    final

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>api response:</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+
+const { data } = await useFetch('/api/ninja')
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
